848eb3373f27cc6cb0cbb4a6c9a14b8b232cbcea
Jan <jan.mitrovic@klarna.com>
2024-03-04 19:28:10 +0100

    Added smooth scroll to gallery link

--- src/app/(general)/components/gallery/images.tsx
+++ src/app/(general)/components/gallery/images.tsx
@@ -9,7 +9,7 @@ interface Props {
 
 export default function Images({ onClick }: Props) {
   return (
-    <main className="viewpoint bg-khaki  px-4 md:px-16 py-10 md:pb-10 md:pt-20">
+    <main className="viewpoint bg-khaki  px-4 md:px-16 py-10 md:pb-10 md:pt-20" id='gallery'>
       <h2 className="text-3xl mb-4 font-semibold text-black">Villa viewpoint</h2>
       <div className="container mb-10 grid max-h-[500px] overflow-y-auto grid-cols-2 gap-4 md:grid-cols-4 lg:grid-cols-7">
         {images.map((image, index) => {
--- src/app/(general)/components/topbar.tsx
+++ src/app/(general)/components/topbar.tsx
@@ -1,4 +1,4 @@
-"use client";
+'use client';
 import React, { RefObject, useState } from 'react';
 import Link from 'next/link';
 import { useRef } from 'react';
@@ -7,58 +7,86 @@ interface Props {
   contactRef: RefObject<HTMLDivElement>;
 }
 
-export default function Topbar({contactRef}: Props) {
+export default function Topbar({ contactRef }: Props) {
   const [showDropdown, setShowDropdown] = useState(false);
 
-  const handleScroll = (ref:HTMLDivElement|null) => {
+  const handleScroll = (ref: HTMLDivElement | null) => {
     if (!ref) return;
     window.scrollTo({
       top: ref.offsetTop,
       left: 0,
-      behavior: "smooth",
+      behavior: 'smooth',
     });
   };
-  
 
   const toggleDropdown = () => {
     setShowDropdown(!showDropdown);
   };
 
   return (
-    <div className="flex h-20 items-center justify-between bg-light-brown md:px-16 px-3 py-2">
-      <div className="flex items-center w-1/3">
-      <Link href="/">
+    <div className="flex h-20 items-center justify-between bg-light-brown px-3 py-2 md:px-16">
+      <div className="flex w-1/3 items-center">
+        <Link href="/">
           <img src="/next.svg" alt="Logo" className="h-8 w-8" />
         </Link>
       </div>
-      <div className="flex items-center space-x-2 text-xs md:text-md lg:text-lg md:space-x-6 font-semibold">
+      <div className="md:text-md flex items-center space-x-2 text-xs font-semibold md:space-x-6 lg:text-lg">
         {/* <button className="text-my-black hover:border-b-2 hover:border-my-black hover:text-gray-800">
           About
         </button> */}
-        <button className="text-my-black hover:border-b-2 hover:border-my-black hover:text-gray-800" onClick={() => handleScroll(contactRef.current)}>
+        <button
+          className="text-my-black hover:border-b-2 hover:border-my-black hover:text-gray-800"
+          onClick={() => handleScroll(contactRef.current)}
+        >
           Contact
         </button>
-        <div className='relative'>
-          <button className="text-my-black hover:border-b-2 hover:border-my-black hover:text-gray-800" onClick={toggleDropdown}>
+        <div className="relative">
+          <button
+            className="text-my-black hover:border-b-2 hover:border-my-black hover:text-gray-800"
+            onClick={toggleDropdown}
+          >
             Language
           </button>
           {showDropdown && (
-            <div className="absolute top-full left-0 mt-1 bg-white rounded-md shadow-lg">
-              <button className="block w-full text-center px-2 md:px-3 py-1 mdtext-sm text-gray-800 hover:bg-gray-200" onClick={() => setShowDropdown(false)}>English</button>
-              <button className="block w-full text-center px-2 md:px-3 py-1 mdtext-sm text-gray-800 hover:bg-gray-200" onClick={() => setShowDropdown(false)}>German</button>
-              <button className="block w-full text-center px-2 md:px-3 py-1 mdtext-sm text-gray-800 hover:bg-gray-200" onClick={() => setShowDropdown(false)}>Croatian</button>
-              <button className="block w-full text-center px-2 md:px-3 py-1 mdtext-sm text-gray-800 hover:bg-gray-200" onClick={() => setShowDropdown(false)}>Slovene</button>
+            <div className="absolute left-0 top-full mt-1 rounded-md bg-white shadow-lg">
+              <button
+                className="mdtext-sm block w-full px-2 py-1 text-center text-gray-800 hover:bg-gray-200 md:px-3"
+                onClick={() => setShowDropdown(false)}
+              >
+                English
+              </button>
+              <button
+                className="mdtext-sm block w-full px-2 py-1 text-center text-gray-800 hover:bg-gray-200 md:px-3"
+                onClick={() => setShowDropdown(false)}
+              >
+                German
+              </button>
+              <button
+                className="mdtext-sm block w-full px-2 py-1 text-center text-gray-800 hover:bg-gray-200 md:px-3"
+                onClick={() => setShowDropdown(false)}
+              >
+                Croatian
+              </button>
+              <button
+                className="mdtext-sm block w-full px-2 py-1 text-center text-gray-800 hover:bg-gray-200 md:px-3"
+                onClick={() => setShowDropdown(false)}
+              >
+                Slovene
+              </button>
             </div>
           )}
         </div>
 
-        <button className="text-my-black hover:border-b-2 hover:border-my-black hover:text-gray-800">
+        <Link
+          className="text-my-black hover:border-b-2 hover:border-my-black hover:text-gray-800"
+          href="#gallery"
+        >
           Gallery
-        </button>
+        </Link>
       </div>
 
-      <div className="flex items-center justify-end text-xs md:text-md lg:text-lg md:text-medium w-1/3">
-        <button className="md:h-10 md:w-32 p-1 rounded-br-lg rounded-tr-lg border-2 border-solid border-my-grey font-bold text-my-grey transition-all hover:scale-105 hover:font-extrabold">
+      <div className="md:text-md md:text-medium flex w-1/3 items-center justify-end text-xs lg:text-lg">
+        <button className="rounded-br-lg rounded-tr-lg border-2 border-solid border-my-grey p-1 font-bold text-my-grey transition-all hover:scale-105 hover:font-extrabold md:h-10 md:w-32">
           Book Now
         </button>
       </div>
