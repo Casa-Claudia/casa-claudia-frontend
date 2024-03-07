2beb307d47f5acf99a53f28e3782148feb6988fe
rokrajher <69865825+rokrajher@users.noreply.github.com>
2024-03-07 13:44:34 +0100

    fixed bookbar

--- src/app/(general)/components/book_bar.tsx
+++ src/app/(general)/components/book_bar.tsx
@@ -1,4 +1,28 @@
+"use client";
+import { useCheckoutState } from "@/state/checkout";
+import { useMemo, useState } from "react";
+import Link from "next/link";
+import Cal from "./calendar/calendar";
+
 export default function BookBar() {
+  const { range, setPrice, setRange } = useCheckoutState();
+  const pricePerNight = 300;
+
+  const firstDate = range[0]?.toLocaleDateString();
+  const secondDate = range[1]?.toLocaleDateString();
+  const timeDiff = Math.round((range[1]?.getTime() - range[0]?.getTime()) / (1000 * 60 * 60 * 24))
+  const isValid = useMemo(() => range[0] !== null && range[1] !== null && range[0]?.getTime() < range[1]?.getTime() && timeDiff > 1, [range]);
+  const selectedDates = isValid? `${firstDate} - ${secondDate}`: 'No dates selected';
+
+  const price = isValid? pricePerNight * timeDiff: 0;
+    // State to control the visibility of the window
+  const [isWindowOpen, setIsWindowOpen] = useState(false);
+
+  const toggleCalendar = () => {
+    // Open the window
+    setIsWindowOpen(!isWindowOpen);
+  };
+
   return (
     <main>
       <div className="relative z-10 hidden md:flex justify-between h-[80px] w-[133%] items-center bg-my-white text-my-grey overflow-visible">
@@ -10,25 +34,26 @@ export default function BookBar() {
           </div>
         </div>
 
-        <div className="flex w-1/3 items-start justify-center bg-my-white px-0.5 py-2">
+        <div className="flex w-2/3 items-start justify-center bg-my-white px-0.5 py-2">
           <img className="mr-2" src="/Calendar.svg" alt="Location" />
-          <div className="flex flex-col justify-start">
-            <button className="mb-0.5 transform font-semibold hover:scale-105">Check In</button>
-            <p className="text-my-light-grey text-sm">5.11.2001</p>
+          <div className="flex flex-col justify-start relative">
+            <button onClick={toggleCalendar} className="mb-0.5 transform font-semibold hover:scale-105">Check in date - Check out date</button>
+            <p className="text-my-light-grey text-sm text-center">{selectedDates}</p>
+            {isWindowOpen && (
+            <div className="mt-5 flex absolute popup top-full left-0 items-center justify-center px-4 bg-white">
+              <div className="container w-[600px] text-black">
+                <Cal />
+              </div>
+            </div>
+            )}
           </div>
         </div>
 
-        <div className="flex w-1/3 items-start justify-center bg-my-white px-0.5 py-2">
-          <img className="mr-2" src="/Calendar.svg" alt="Location" />
-          <div className="items-left flex flex-col justify-center">
-            <button className="mb-0.5 transform font-semibold hover:scale-105">Check Out</button>
-            <p className="text-my-light-grey text-sm">6.11.2001</p>
-          </div>
-        </div>
       <div className=" w-1/3 h-[80px] flex items-center justify-center bg-my-grey px-0.5  py-2">
-        <button className="mb-0.5 transform text-lg text-my-white hover:scale-105 hover:font-semibold">
+        <Link className="mb-0.5 transform text-lg text-my-white hover:scale-105 hover:font-semibold"
+        href={"/#booker"}>
           Book Now
-        </button>
+        </Link>
       </div>
       </div>
     </main>
