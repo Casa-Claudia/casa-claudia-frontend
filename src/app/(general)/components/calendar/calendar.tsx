9a7d0eb0be5f644e6f82daec9ce6d53b0652cd72
rokrajher <69865825+rokrajher@users.noreply.github.com>
2024-03-07 10:43:11 +0100

    finished booking logic--need testing!!

--- src/app/(general)/components/calendar/calendar.tsx
+++ src/app/(general)/components/calendar/calendar.tsx
@@ -5,20 +5,29 @@ import Calendar from 'react-calendar';
 import './styles.css';
 import { useCheckoutState } from '@/state/checkout';
 
+import { ReservedDates } from './data';
+import { DateRange } from './data';
+
 export default function Cal() {
   const { range, setRange } = useCheckoutState();
-  const fullDates = [ new Date(2024,2,16)];
   
-  const disableDates= (date:Date, fullDates:Date[]) => {
+  const disableDates= (date:Date, ReservedDates: DateRange[]) => {
     if (date.getDay() != 6) return true;
-    return fullDates.some(fullDate => date.getTime() === fullDate.getTime());
+    // disale the days between the ranges
+    return ReservedDates.some(({ start, end }) => 
+      date.getTime() > start.getTime() && date.getTime() < end.getTime()
+    );
+
   };
 
-  const checkIfValidRange = (range:[Date, Date], fullDates: Date[], setRange: (range: [Date, Date]) => void) => {
+  const checkIfValidRange = (range:[Date, Date], ReservedDates: DateRange[], setRange: (range: [Date, Date]) => void) => {
     const [start, end] = range;
     if (!start || !end) return;
   
-    const isInvalidRange = fullDates.some(date => start.getTime() < date.getTime() && end.getTime() > date.getTime());
+    const isInvalidRange = ReservedDates.some(({ start:s, end: e }) => 
+    start.getTime()<=s.getTime() && end.getTime()>s.getTime() 
+    && start.getTime()<e.getTime() && end.getTime()>=e.getTime()
+    );
   
     if (isInvalidRange) {
       console.log('not valid range');
@@ -35,13 +44,13 @@ export default function Cal() {
       <Calendar
         value={range}
         showDoubleView={true}
-        onChange={(e: any) => checkIfValidRange(e, fullDates, setRange)}
+        onChange={(e: any) => checkIfValidRange(e, ReservedDates, setRange)}
         selectRange={true}
         maxDate={new Date(2024, 11, 31)}
         minDate={new Date()}
         next2Label={null}
         prev2Label={null}
-        tileDisabled={	({ activeStartDate, date, view }) => disableDates(date, fullDates)}
+        tileDisabled={	({ activeStartDate, date, view }) => disableDates(date, ReservedDates)}
         allowPartialRange={true}
       />
     </div>
---
+++ src/app/(general)/components/calendar/data.ts
@@ -0,0 +1,16 @@
+export interface DateRange {
+    start: Date;
+    end: Date;
+}
+
+
+export const ReservedDates: DateRange[] = [
+    {
+        start: new Date(2024, 2, 9),
+        end: new Date(2024, 2, 23)
+    },
+    {
+        start: new Date(2024, 3, 13),
+        end: new Date(2024, 3, 27)
+    }
+];
