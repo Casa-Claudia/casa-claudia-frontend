4737c2412211b8bf68cb1041add596b9f2e42e1e
rokrajher <69865825+rokrajher@users.noreply.github.com>
2024-03-11 18:22:35 +0100

    some order updates

--- src/app/order/page.tsx
+++ src/app/order/page.tsx
@@ -10,6 +10,16 @@ export default function Order() {
   return (
     <div className="order">
       <TopbarCheckout />
+      <main className="bg-my-white md:px-32 px-4 py-16">
+      <h1 className="text-3xl font-bold text-black">Thank you for your oder</h1>
+      <p className="mt-5  text-black">
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quis suscipit maxime esse quaerat
+        reprehenderit quibusdam molestias iusto facere optio alias expedita itaque aliquam aut,
+        cumque delectus, doloremque facilis sunt saepe ab. Porro ex voluptatum tempora,
+        exercitationem quas in eum quisquam atque perferendis sunt itaque animi rem quaerat minima
+        quia. Amet?
+      </p>
+    </main>
       <Footer />
     </div>
   );
