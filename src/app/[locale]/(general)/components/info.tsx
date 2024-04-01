29f7f916ef5e7c93116bb460e7598f3af22e7bac
rokrajher <69865825+rokrajher@users.noreply.github.com>
2024-04-01 15:25:17 +0200

    bed sizing fixed

--- src/app/[locale]/(general)/components/info.tsx
+++ src/app/[locale]/(general)/components/info.tsx
@@ -51,7 +51,7 @@ export default function Info({t}: Props) {
             </div>
             <div className=" ml-6 py-2 text-sm text-my-light-grey ">
               <ul className="list-disc">
-                <li>{t("info-king-size")}</li>
+                <li>{t("info-king-size")} 180x200</li>
                 <li>{t("info-in-suite-bath")}</li>
                 <li>TV</li>
                 <li>{t("info-terrace")}</li>
@@ -67,7 +67,7 @@ export default function Info({t}: Props) {
             </div>
             <div className=" ml-6 py-2 text-sm text-my-light-grey ">
               <ul className="list-disc">
-                <li>{t("info-king-size")}</li>
+                <li>{t("info-king-size")} 160x200</li>
                 <li>{t("info-hifi")}</li>
                 <li>TV</li>
                 <li>{t("info-fireplace")}</li>
